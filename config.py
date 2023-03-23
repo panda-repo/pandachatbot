6fae778ddc6f8a6a3abd62b1fe0a9f47ec9682b1
panda-repo <128521829+panda-repo@users.noreply.github.com>
2023-03-23 23:32:01 +0700

    Create config.py

---
+++ config.py
@@ -0,0 +1,29 @@
+from os import getenv
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+# Get it from my.telegram.org
+API_ID = int(getenv("API_ID"))
+API_HASH = getenv("API_HASH")
+
+## Get it from @Botfather in Telegram.
+BOT_TOKEN = getenv("BOT_TOKEN")
+
+# SUDO USERS
+SUDO_USER = list(
+    map(int, getenv("SUDO_USER", "").split())
+)  # Input type must be interger
+
+# You'll need a Private Group ID for this.
+LOG_GROUP_ID = int(getenv("LOG_GROUP_ID"))
+
+# Message to display when someone starts your bot
+PRIVATE_START_MESSAGE = getenv(
+    "PRIVATE_START_MESSAGE",
+    "Hello! Welcome to my Personal Assistant Bot",
+)
+
+# Database to save your chats and stats... Get MongoDB:-  https://notreallyshikhar.gitbook.io/yukkimusicbot/deployment/mongodb#4.-youll-see-a-deploy-cloud-database-option.-please-select-shared-hosting-under-free-plan-here
+MONGO_DB_URI = getenv("MONGO_DB_URI", None)
